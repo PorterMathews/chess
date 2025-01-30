351ffb4f8cf4315f8c285cda65438927126a05eb
Porter Mathews <porterlikesbacon@gmail.com>
2025-01-29 21:00:46 -0700

    implimented first function

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,16 +10,19 @@ import java.util.Collection;
  */
 public class ChessGame {
 
-    public ChessGame() {
+    private TeamColor teamTurn;
+    private ChessBoard chessBoard;
 
+    public ChessGame() {
+        teamTurn = TeamColor.WHITE;
+        chessBoard = new ChessBoard();
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-
-        throw new RuntimeException("Not implemented");
+        return this.teamTurn;
     }
 
     /**
@@ -28,8 +31,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-
-                throw new RuntimeException("Not implemented");
+        this.teamTurn = team;
     }
 
     /**
@@ -101,8 +103,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-
-        throw new RuntimeException("Not implemented");
+        chessBoard = board;
     }
 
     /**
@@ -111,7 +112,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-
-        throw new RuntimeException("Not implemented");
+        return chessBoard;
     }
 }
